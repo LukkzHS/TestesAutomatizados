27798e1775d9fb6b355b5226ef0385dcacb8c5cb
Lucca <136863244+LukkzHS@users.noreply.github.com>
2024-09-02 20:32:36 -0300

    S1196 || S5786
    S1196: Implementei o contextLoads para sanar o erro de método vazio.
    S5786: Removi os "public" dos testes criados anteriormente.

--- A2/calculadora/src/test/java/com/example/calculadora/CalculadoraApplicationTests.java
+++ A2/calculadora/src/test/java/com/example/calculadora/CalculadoraApplicationTests.java
@@ -1,68 +1,85 @@
 package com.example.calculadora;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CalculadoraApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
+
+    // S1186
     @Test
     void contextLoads() {
+        assertThat(context).isNotNull();
     }
 
+
+    // S5786 
     @Test
-    public void testSomarNumeroNegativo() {
+    void testSomarNumeroNegativo() {
         Calculadora calc = new Calculadora(5);
         calc.somar(-3);
         assertEquals(2, calc.getMemoria());
     }
 
+    // S5786
     @Test
-    public void testSubtrairNumeroPositivo() {
+    void testSubtrairNumeroPositivo() {
         Calculadora calc = new Calculadora(10);
         calc.subtrair(4);
         assertEquals(6, calc.getMemoria());
     }
 
+    // S5786
     @Test
-    public void testMultiplicarNumeroPositivo() {
+    void testMultiplicarNumeroPositivo() {
         Calculadora calc = new Calculadora(4);
         calc.multiplicar(3);
         assertEquals(12, calc.getMemoria());
     }
 
+    // S5786
     @Test
-    public void testDividirPorZero() {
+     void testDividirPorZero() {
         Calculadora calc = new Calculadora(6);
         assertThrows(Exception.class, () -> calc.dividir(0));
     }
 
+    // S5786
     @Test
-    public void testDividirPorValorPositivo() throws Exception {
+     void testDividirPorValorPositivo() throws Exception {
         Calculadora calc = new Calculadora(20);
         calc.dividir(5);
         assertEquals(4, calc.getMemoria());
     }
 
+    // S5786
     @Test
-    public void testExponenciacaoPor1() throws Exception {
+     void testExponenciacaoPor1() throws Exception {
         Calculadora calc = new Calculadora(3);
         calc.exponenciar(1);
         assertEquals(3, calc.getMemoria());
     }
 
+    // S5786
     @Test
-    public void testExponenciacaoPor10() throws Exception {
+     void testExponenciacaoPor10() throws Exception {
         Calculadora calc = new Calculadora(3);
         calc.exponenciar(10);
         assertEquals(59049, calc.getMemoria());
     }
 
+    // S5786
     @Test
-    public void testZerarMemoria() {
+     void testZerarMemoria() {
         Calculadora calc = new Calculadora(3);
         calc.zerarMemoria();
         assertEquals(0, calc.getMemoria());
